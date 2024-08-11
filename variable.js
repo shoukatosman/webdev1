70db6a3bc7e1e057d547b08b3bef9db2c80c501d
shoukatosman <shoukatosman10@gmail.com>
2024-08-11 23:04:22 +0600

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,74 @@
+// var money = 3456; // numeric
+// var name = "ali shoirf"; // string
+// var isSunny = true; // boolean
+// var product99 = "Balti";
+// // var PRODUCT = "BOOK" NOT ALLOW
+// var PIE = 3.1416
+// var onion = 120;
+// var potato = 70;
+// var total = onion + potato;
+// console.log(total);
+
+// var minus = onion - potato;
+// console.log(minus);
+
+// var multiply = onion * potato;
+// console.log(multiply);
+
+// var divided = onion / potato;
+// console.log(divided);
+
+// var sandle = "9900.99";
+// var convert = parseInt(sandle);
+// console.log(convert);
+
+// var cup = 12 // integar
+
+// var floatNumber = 34567.23;
+// var integarNumber = 23456;
+// var result = floatNumber + integarNumber;
+// console.log(result);
+
+var cup = 12 // integar
+
+// var floatNumber = "34567.23";
+// var floatNumberConvert = parseFloat(floatNumber)
+// var integarNumber = 23456;
+// var result = floatNumberConvert + integarNumber;
+// console.log(result);
+
+// var cup = "12";
+// var integarConvert = parseInt(cup)
+// console.log(integarConvert);
+
+// var a = 0.1;
+// var b = 0.2;
+// var total = a + b;
+// console.log(parseFloat(total.toFixed(1)));
+
+var date = new Date();
+console.log(date);
+
+var time = new Date();
+console.log(time);
+
+var hours = time.getHours();
+console.log(hours);
+
+var minutes = time.getMinutes();
+console.log(minutes);
+
+var seconds = time.getSeconds();
+console.log(seconds);
+
+var currentYear = new Date().getFullYear();
+console.log(currentYear);
+
+var currentMonth = new Date().getMonth() + 1;
+console.log(currentMonth);
+
+var currentDay = new Date().getDate();
+console.log(currentDay);
+
+
+
