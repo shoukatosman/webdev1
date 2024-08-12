d9e52f19d77c116c6d216412b4699f7f5459b304
shoukatosman <shoukatosman10@gmail.com>
2024-08-12 21:44:13 +0600

    Add files via upload

---
+++ array.js
@@ -0,0 +1,20 @@
+// var students = ['osman', 'abdur rahman', 'brithi', 'shaikat',];
+// console.log(students);
+
+// var students = ['osman', 'abdur rahman', 'brithi', 'bapparaj',];
+// var index = students.indexOf('brithi');
+// console.log(index);
+
+// var studentId = [ 12, 13, 444,  5643636, 25252, 98, 46, 6, 3, 67, 23, 54, 23, 63, 62, 87, 23, 97, 85,]
+// var ten = studentId[10];
+// studentId[10] = 9999;
+// console.log(studentId);
+
+var students = ['osman', 'abdur rahman', 'brithi', 'bapparaj',];
+// students.push('hero alom');
+// students.push('Shamim ahmed');
+// students.push('12345');
+students.pop()
+students.pop()
+students.pop()
+console.log(students);
